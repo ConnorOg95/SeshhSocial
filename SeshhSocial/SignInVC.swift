c93208b97bd079d7d23a746764276dd81c46c5d8
ConnorOg95 <connorog95@gmail.com>
2017-02-13 23:17:36 +0000

    Completed email auth

--- SeshhSocial/SignInVC.swift
+++ SeshhSocial/SignInVC.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import Firebase
 
 class SignInVC: UIViewController {
-
+    
+    @IBOutlet weak var emailTxt: ModifiedTextField!
+    @IBOutlet weak var passwordTxt: ModifiedTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,35 @@ class SignInVC: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+//    func firebaseAuth(_ credential: FIRAuthCredential) {
+//        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+//            if error != nil {
+//                print("CONNOR: Unable to authenticate with Firebase - \(error)")
+//            } else {
+//                print("CONNOR: Successfully authenticated with Firebase")
+//            }
+//        })
+//    }
 
+    @IBAction func logInBtnPressed(_ sender: Any) {
+        
+        if let email = emailTxt.text, let password = passwordTxt.text {
+            FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
+                if error == nil {
+                    print("CONNOR: Email user authenticated with Firebase")
+                } else {
+                    FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
+                        if error != nil {
+                            print("CONNOR: Unable to authenticate with Firebase Email")
+                        } else {
+                            print("CONNOR: Sucessfully authenticated with Firebase Email")
+                        }
+                    })
+                }
+            })
+        }
+    }
 
 }
 
